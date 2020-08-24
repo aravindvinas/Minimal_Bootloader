97aef8907579f60985f4b5b49eceafbb4e514f29
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-08-24 22:38:10 +0530

    Main program to read binary data

--- Host/Source/main.c
+++ Host/Source/main.c
@@ -1,88 +1,56 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
 
-int
-main(void)
+
+//file total length
+uint32_t file_len;
+
+int 
+main(int argc, char* argv[])
 {
-	uint8_t data[256], rx;
-	uint32_t crc_data;
+	//Packet buffer
+	uint8_t packet[256];
+	//left out data
+	uint32_t data_len = 0;
+	//amount of data to read
+	uint32_t data_read = 0;
+	//amount of data read
+	uint32_t read_len = 0;
 
-	const uint32_t addr = 0x08000800;
-	const char file[20];
-	const char port[20];
-	printf("Enter Flash base addr\n");
-	scanf("%s\n", addr);
-	printf("Enter file name\n");
-	scanf("%s\n", file);
-	printf("Enter port address\n");
-	scanf("%s\n", port);
+	//get total file length	
+	FILE* fp = fopen(argv[1], "rb");
 
-	port_handle hport;
-
-	serial_init(&hport, port);
-
-	// First send the address in which the code is to be flashed along with its CRC
-	//Check for ACK 
-	//If ACK proceed with reading the file and storing it in the buffer, else resend address
-	//Check ACK else resend buffer
-
-	/*break_down(addr, data);
-	crc_data = crc(data, 4);
-	break_down(crc_data, (data + 5));
-	
-	serial_send(&hport, &data, 8);
-	if(serial_read(&hport, &rx, 1)){
-		printf("Address CRC failed");
+	//if file opening encounters error
+	if(errno){
+		perror("Error");
+		printf("Exiting program!\r\n");
 		exit(0);
 	}
 
-	fops_fcpy(data, file);
-	uint32_t data_crc = crc(data, 224);
-	break_down(data_crc, (data + 225));
+	perror("Open Status");
 
-	serial_send(&hport, data, 256);*/
-}
+	//Compute file length
+	fseek(fp, 0, SEEK_END);
+	file_len = ftell(fp);
+	data_len = file_len;
+	fseek(fp, 0, SEEK_SET);
+	printf("Total length of the file: %d\n", file_len);
 
-void break_down(uint32_t data, uint8_t* ptr)
-{
-	uint32_t temp = data;
+	//reading the file
+	while(data_len){
+		if(data_len > 244)
+			data_read = 244;
+		else
+			data_read = data_len;
 
-	temp = temp & 0xFF000000UL;
-	temp = (temp >> 24);
-	ptr[0] = (uint8_t) temp;
+		read_len = fread((void*)packet, 1, data_read, fp);
+		data_len = data_len - read_len;
 
-	temp = data;
-	temp = temp & 0x00FF0000UL;
-	temp = (temp >> 16);
-	ptr[1] = (uint8_t) temp;
-
-	temp = data;
-	temp = temp & 0x0000FF00UL;
-	temp = (temp >> 8);
-	ptr[2] = (uint8_t) temp;
-
-	temp = data;
-	temp = temp & 0x000000FFUL;
-	ptr[3] = (uint8_t) temp;
-}
-
-
-uint32_t crc(uint8_t* buf, uint32_t size)
-{
-	int i, n; 
-	uint32_t crc = 0xFFFFFFFF;
-	for(n=0; n<size; n++)
-	{
-		uint32_t data = buf[n];
-		crc = crc ^ data;
-		for(i=0; i<32; i++)
-		{
-			if(crc & 0x80000000){
-				crc = (crc << 1) ^ 0x04C11DB7;
-			}
-			else{
-				crc = (crc << 1);
-			}
-		}
+		printf("Data read: %d  |  Data yet to read: %d\n", read_len, data_len);
 	}
-	return crc;
+
+	fclose(fp);
+	
+	return 1;
 }
