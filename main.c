020e23bda5abf51385fa5b914ba8708c083c68f6
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-08-30 23:49:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,113 +1,8 @@
-/*
- *							BOOTLOADER PROTOCOL
- *
- * Handshake with STM32:
- *			Send 0xdead and receive 0xbeef
- *			Receive timeout 10sec; resend three times; exit(0)
- *
- * Binary Size and Flash base addr:
- *		Send binary size and receive ack (0xacd)
- *		Receive timeout 10sec; resend three times; exit(0)
- *
- * Initiate Tx signal:
- *		Receive ack(0xacd) 
- *		Receive timeout 10sec; No resend; exit(0)
- *
- * Send 248 bytes of hex + 8 bytes CRC:
- *		Receive ACK for CRC
- *			Receive timeout 10sec; resend 3 times; exit(0)
- *		Flash write ACK
- *			Receive timeout 10sec; no resend; exit(0)
- *
- * Finish transmitting binary
- *
- * Send signal to jump user code
- *
- * After jump to user code close serial connection
- * exit(0)
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <time.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <unistd.h>
-#include <errno.h>
-
-
-//Receive Status
-#define RX_TIMEOUT_ERR	0
-#define RX_ACK_FAIL	-1
-#define RX_ACK_SUCCESS	1
-
-
-uint32_t file_len;		//Total file length
-unsigned char* ack = "\xBE\xEF";
-unsigned char txBuffer[256];	//Transfer Buffer	
-unsigned char rxBuffer[10];		//Receive Buffer	
-int port;
-int read_count = 0;			//serial read count
-int write_count = 0;		//serial write count
-
-int config_port(char* str);
-void handshake(int port);
-void tx_binInfo(int port, char* bin, char* flash_base);
-int rx_Status(void);
-int time_lapse(clock_t end, clock_t start);	//Calculate time between Start and End
-
-//argv[1] is binary file
-//argv[2] is serial port addr
-//argv[3] is Flash base addr
-int 
-main(int argc, char* argv[])
-{
-
-	if(argc < 4){
-		printf("Insufficient Arguments\n");
-		printf("Exiting Program\n");
-		exit(0);
 	}
-	
-	printf("*************************\n");
-	printf("*                       *\n");
-	printf("*     Flash Loader      *\n");
-	printf("*                       *\n");
-	printf("*************************\n");
-	
-	port = config_port(argv[2]);				//Configure Serial Port
-	printf("Serial configured successfully\n");
-	
-	handshake(port);		//Handshake with STM32
 
-	//tx_binInfo(port, argv[1], argv[3]);		//Send bin size and flash base addr
-	
+	tcflush(port, TCIOFLUSH);
 	close(port);
 
-	//reading the file
-/*	while(data_len){
-		if(data_len > 256)
-			data_read = 256;
-		else
-			data_read = data_len;
-
-		read_len = fread((void*)packet, 1, data_read, fp);
-		data_len = data_len - read_len;
-
-		printf("Data read: %d  |  Data yet to read: %d\n", read_len, data_len);
-		for(int i=0; i<read_len; i++)
-		{
-			printf("%x", packet[i]);
-			printf("\n");
-		}
-
-	}
-
-	fclose(fp);
-*/
-	
 	return 1;
 }
 
@@ -146,9 +41,9 @@ int config_port(char* str)
 
 	pConfig.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);	//Non-Canonical Mode
 
-	//Min data received and maximum wait time
-	pConfig.c_cc[VMIN] = 0;		//1 byte min
-	pConfig.c_cc[VTIME] = 0;	//port timeout 0.1sec
+	//Poll once
+	pConfig.c_cc[VMIN] = 0;		
+	pConfig.c_cc[VTIME] = 0;	
 
 	//set the attributes immediately
 	tcsetattr(port, TCSANOW, &pConfig);
@@ -167,21 +62,23 @@ void handshake(int port)
 	
 	//Sending data 0xDEAD
 	memcpy(txBuffer, "\xDE\xAD", 2);
-	write_count = write(port, txBuffer, 2);			//Handshake attempt 1
-	if(write_count < 0){							//Error in sending handshake to STM32
+	tcflush(port, TCIOFLUSH);
+	write_count = write(port, txBuffer, 2);	//Handshake attempt 1
+	if(write_count < 0){	//Error in sending handshake to STM32
 		perror("Error");
 		printf("Restransmitting...");
-		write_count = write(port, txBuffer, 2);		//Handshake attemp 2
+		tcflush(port, TCOFLUSH);
+		write_count = write(port, txBuffer, 2);	//Handshake attempt 2
 		if(write_count < 0){
 			perror("Error in transmission");
-			printf("Exiting the program!!!");		//Error in attempt 2 handshake and exit program
+			printf("Exiting the program!!!");	//Error in attempt 2 handshake and exit program
 			exit(0);
 		}
 	}
 
 	printf("Sent %d bytes\n", write_count);
 
-	rxStatus = rx_Status();		//wait for reply from STM32
+	rxStatus = rx_Status();	//wait for reply from STM32
 
 	if(rxStatus == RX_TIMEOUT_ERR)
 		exit(0);
@@ -201,9 +98,11 @@ void tx_binInfo(int port, char* bin, char* flash_base)
 {
 	int rxStatus = 0;
 
-	FILE* fp = fopen(bin, "rb");	//Open bin file in binary mode
+	/***Sending Bin file size***/
 
-	if(errno){		//Error opening bin file
+	/*FILE* fp = fopen(bin, "rb");	//Open bin file in binary mode
+
+	if(errno){	//Error opening bin file
 		perror("Error");
 		printf("Exiting program!!!\n");
 		exit(0);
@@ -211,7 +110,7 @@ void tx_binInfo(int port, char* bin, char* flash_base)
 
 	perror("File Open Status");
 
-	fseek(fp, 0, SEEK_END);		//Compute bin file size
+	fseek(fp, 0, SEEK_END);	//Compute bin file size
 	file_len = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
 	printf("Total length of the file: %d\n", file_len);
@@ -219,6 +118,12 @@ void tx_binInfo(int port, char* bin, char* flash_base)
 	int conv_len = snprintf(NULL, 0, "{data:%d}", file_len);
 	sprintf((char*)txBuffer, "%d", file_len);
 
+	write_count = 0;
+	tcflush(port, TCIOFLUSH); //flush before write
+
+	write_count = write(port, &txBuffer[0], conv_len);	//sending length of data that follows
+
+	write_count = 0;	//send bin file size
 	write_count = write(port, txBuffer, conv_len);	//Writing file size
 	if(write_count < 0){	
 		perror("Error");
@@ -240,9 +145,15 @@ void tx_binInfo(int port, char* bin, char* flash_base)
 		printf("Binary Size ACK fail...\n");
 		printf("Exiting Program !!!");
 		exit(0);
-	}
+	}*/
+
+	/******************************************/
 	
-	strcpy((char*)txBuffer, flash_base);
+	rxStatus = 0;	//rxStatus reset
+
+	strcpy((char*)txBuffer, flash_base);	//copy flash base addr to txBuffer
+
+	tcflush(port, TCIOFLUSH);	//flush before write
 	write_count = write(port, txBuffer, conv_len);	//Writing Flash Base addr
 	if(write_count < 0){	
 		perror("Error");
@@ -273,12 +184,40 @@ void tx_binInfo(int port, char* bin, char* flash_base)
 	fclose(fp);	//Close the bin file
 }
 
+int file_send(int port, char* bin)
+{
+	FILE* fp = fopen(bin, "rb");	//Open bin file in binary mode
+
+	if(errno){		//Error opening bin file
+		perror("Error");
+		printf("Exiting program!!!\n");
+		exit(0);
+	}
+
+	//reads 255 bytes worth of data or how much ever is remaining
+	if(data_len > 255)
+		data_read = 255;
+	else
+		data_read = data_len;
+		
+	read_len = fread((void*)txBuffer, 1, data_read, fp);
+	data_len = data_len - read_len;
+
+	//calculate CRC and append at end of buffer
+	crc(read_len, (int)read_len);
+	txbuffer[255] = crc_val;
+	
+	tcflush(port, TCIOFLUSH);
+	write_count = write(port, txBuffer, 256);
+
+	return rxStatus();
+}
+
 int rx_Status(void)
 {
 
-	//reset read count and flush input port buffer
+	//reset read count 
 	read_count = 0;
-	tcflush(port, TCIFLUSH);
 
 	clock_t start =  clock();	//Start time
 	while(time_lapse(clock(), start) < 10){
@@ -294,12 +233,33 @@ int rx_Status(void)
 		return RX_TIMEOUT_ERR;
 	}
 
-	//Handshake ACK failed
+	//ACK failed
 	if(strncmp((char*)rxBuffer, (char*)ack, 2)){
 		return RX_ACK_FAIL;
 	}
 
 	return RX_ACK_SUCCESS;
+}
+
+uint8_t crc(uint32_t data, int read_len)
+{
+	crc_val = 0xFF;
+	uint8_t data;	//CRC of the data being calculated
+
+	for(int n=0; n<read_len; n++)
+	{
+		uint8_t data =	data[n];
+
+		for(int i=0; i<8; i++)
+		{
+			if(crc_val ^ 0x80)
+				crc_val = (crc_val << 1) ^ poly;
+			else
+				crc_val = (crc_val << 1);
+		}
+	}
+
+	return crc_val;
 }
 
 int time_lapse(clock_t end, clock_t start)
@@ -308,3 +268,5 @@ int time_lapse(clock_t end, clock_t start)
 	time = (int)((end - start)/CLOCKS_PER_SEC);
 	return time;
 }
+
+/***************End of File***************/
